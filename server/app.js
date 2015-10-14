3066914c2624888808f9ab23ed351c6087122e59
MedhatDawoud <medhatdawoud@gmail.com>
2015-10-14 21:30:39 +0200

    Added initial code for server side application

---
+++ server/app.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/*
+ * Express Dependencies
+ */
+var express = require('express');
+var app = express();
+var port = 3000;
+
+// For gzip compression
+app.use(express.compress());
+
+/*
+ * Config for Production and Development
+ */
+if (process.env.NODE_ENV === 'production') {
+    // Locate the views
+    app.set('views', '../client');
+    
+    // Locate the assets
+    app.use(express.static('../client'));
+
+} else {
+    // Locate the views
+    app.set('views', '../client');
+    
+    // Locate the assets
+    app.use(express.static('../client'));
+}
+
+/*
+ * Routes
+ */
+// Index Page
+app.get('/', function(request, response, next) {
+    response.write('index');
+});
+
+
+/*
+ * Start it up
+ */
+app.listen(process.env.PORT || port);
+console.log('Express started on port ' + port);
