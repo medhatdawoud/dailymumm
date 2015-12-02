eeed60b98924aee5698952df96f8550d5444bdd9
MedhatDawoud <medhatdawoud@gmail.com>
2015-12-03 00:38:54 +0200

    Styling the homepage and creating the background video as needed, added default background image, added new 2 fonts to be used globally in the application, added a new folder in scss folder to hold styling of pages, Enhance the style of the signup directive, close #4 issue

--- client/app/scripts/controllers/homeCtrl.js
+++ client/app/scripts/controllers/homeCtrl.js
@@ -10,7 +10,15 @@
     function HomeController($scope) {
         var vm = this;
         
-        vm.title = "My Homepage";
+        vm.title = "Daily Mumm";
+
+        vm.about = "Daily Mumm is a web application that enable you and your colleges to order foods with each others without duplicating the same processes in the same day within different individuals in th same place, order together is simple and easy.";
         
+        vm.steps = [
+        	"Login or Create a new account",
+        	"Join a group or create a one to order food together",
+        	"Choose a restaurant and start an order",
+        	"enjoy tracking your orders with others' orders"
+        ];
     }
 })();
