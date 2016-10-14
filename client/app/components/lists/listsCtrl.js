c268082208b72a042e94c6fabc29ef463e1bf376
Medhat Dawoud <medhatdawoud@gmail.com>
2016-10-14 02:17:31 +0200

    Remove dummy data and bind lists from database enhance styling of lists hide legend when no lists created general styles scss file

--- client/app/components/lists/listsCtrl.js
+++ client/app/components/lists/listsCtrl.js
@@ -5,11 +5,36 @@
         .module('dailyMummApp')
         .controller('ListsDirectiveCtrl', ListsDirectiveController);
 
-    ListsDirectiveController.$inject = ['$scope', 'ListsService', '$timeout', '$rootScope'];
+    ListsDirectiveController.$inject = ['$scope', 'ListsService', '$timeout', '$rootScope', 'AuthService'];
 
-    function ListsDirectiveController($scope, ListsService, $timeout, $rootScope) {
+    function ListsDirectiveController($scope, ListsService, $timeout, $rootScope, AuthService) {
         var lvm = this;
-        lvm.listOfGroups = ListsService.getLists();
+        lvm.listOfGroups = [];
+        lvm.userData = AuthService.getCurrentUserInfo();
+
+        lvm.$onInit = getLists();
+
+        function getLists() {
+            var userId = lvm.userData.id;
+            ListsService.getLists(userId, function (response) {
+                if (response.success) {
+                    var newLists = [];
+                    response.data.forEach(function (item) {
+                        var list = {
+                            id: item._id,
+                            name: item.name,
+                            createdAt: item.created_at,
+                            picturePath: item.picturePath,
+                            owned: item.owner.id === userId,
+                            owner: item.owner,
+                            subscribers: item.subscribers
+                        };
+                        newLists.push(list);
+                    });
+                    lvm.listOfGroups = newLists;
+                }
+            });
+        }
 
         $timeout(function () {
             if ($("a.one-list").length) {
--- client/app/services/lists/lists.service.js
+++ client/app/services/lists/lists.service.js
@@ -12,48 +12,14 @@
 
         service.getLists = getLists;
 
-        function getLists() {
-            return [
-                { 
-                    id: 1, 
-                    name: "My List 1", 
-                    picture: "assets/images/lists/list1.jpg", 
-                    subscribers: [
-                        { id: 1111, username: "medhatdawoud", fullname: "Medhat Dawoud", subscribtionDate: "13 Oct 2016", owner: true}
-                    ], 
-                    owned: true 
-                },
-                { 
-                    id: 2, 
-                    name: "My List 2", 
-                    picture: "assets/images/lists/list2.jpg", 
-                    subscribers: [
-                        { id: 1111, username: "medhatdawoud", owner: false}
-                    ], 
-                    owned: false 
-                },
-                { 
-                    id: 3, 
-                    name: "My List 3", 
-                    picture: "assets/images/lists/list3.jpg", 
-                    subscribers: [
-                        { id: 1111, username: "medhatdawoud", owner: false}
-                    ], 
-                    owned: false 
-                },
-                { 
-                    id: 4, 
-                    name: "My List 4", 
-                    picture: "assets/images/lists/list4.jpg", 
-                    subscribers: [
-                        { id: 1111, username: "medhatdawoud", owner: false}
-                    ], 
-                    owned: false 
-                }
-            ];
+        function getLists(userId, callback) {
+            $http.get(apiServer + '/api/lists/byuser', { params: { userId: userId } })
+                .then(function (response) {
+                    callback({ success: true, data: response.data });
+                }, function (response) {
+                    callback({ success: false, code: response.data });
+                });
         }
-        
-        
 
         return service;
     }
