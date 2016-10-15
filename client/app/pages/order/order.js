53e3713e520ed4fee31d2b8c3e682795896a03b8
Medhat Dawoud <medhatdawoud@gmail.com>
2016-10-15 14:09:00 +0200

    Bind lists to the order page bind the selected action

--- client/app/pages/order/order.js
+++ client/app/pages/order/order.js
@@ -5,18 +5,53 @@
         .module('dailyMummApp')
         .controller('OrderCtrl', OrderController);
 
-    OrderController.$inject = ['$scope', '$rootScope', '$state', 'ListsService', 'RestaurantsService', '$timeout', 'CountDownService'];
+    OrderController.$inject = ['$scope', '$rootScope', '$state', 'ListsService', 'RestaurantsService', '$timeout', 'CountDownService', 'AuthService'];
 
-    function OrderController($scope, $rootScope, $state, ListsService, RestaurantsService, $timeout, CountDownService) {
+    function OrderController($scope, $rootScope, $state, ListsService, RestaurantsService, $timeout, CountDownService, AuthService) {
         var vm = this;
 
-        vm.listOfGroups = ListsService.getLists();
-
+        vm.userData = AuthService.getCurrentUserInfo();
+        vm.$onInit = getLists();
+        vm.listOfGroups = [];
         vm.restaurants = RestaurantsService.getRestaurants();
-
         vm.changeRestaurant = changeRestaurant;
-
         vm.startOrder = startOrder;
+        vm.listSelected = listSelected;
+        vm.selectedList = null;
+
+        function listSelected(list){
+            vm.listOfGroups.forEach(function(item){
+                if(item.id != list.id)
+                    item.selected = false;
+            });
+            vm.selectedList = list;
+            console.log(vm.listOfGroups);
+        }
+
+        function getLists(callback) {
+            var userId = vm.userData.id;
+            ListsService.getLists(userId, function (response) {
+                if (response.success) {
+                    var newLists = [];
+                    response.data.forEach(function (item) {
+                        var list = {
+                            id: item._id,
+                            name: item.name,
+                            createdAt: item.created_at,
+                            picturePath: item.picturePath,
+                            owned: item.owner.id === userId,
+                            owner: item.owner,
+                            subscribers: item.subscribers
+                        };
+                        newLists.push(list);
+                    });
+                    vm.listOfGroups = newLists;
+
+                    if (typeof (callback) == "function")
+                        callback();
+                }
+            });
+        }
 
         function startOrder() {
             var confirmed = confirm("By clicking on this button, you will start order from the selected restaurant. \n \n Are you sure ?");
