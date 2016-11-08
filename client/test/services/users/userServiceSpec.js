d7bb2ccfe94577e8f199e3b7b682136d37400ab2
Medhat Dawoud <medhatdawoud@gmail.com>
2016-11-08 17:04:46 +0200

    fix issue with unit testing user service, issue #32

--- client/test/services/users/userServiceSpec.js
+++ client/test/services/users/userServiceSpec.js
@@ -1,33 +1,41 @@
 'use strict';
 
-describe('UserService service', function () {
-
+describe('UserService service', function() {
+    var UserService, $httpBackend;
     var mockRegisterData = {
         username: 'medhatdawoud',
         email: "medhatdawoud@gmail.com",
         password: 123456
     };
 
-    var httpApiServer = 'http://localhost';
-
-    beforeEach(function () {
+    beforeEach(function() {
         module('dailyMummApp');
     });
-    
-    it('Should issue a POST request to /api/user when createnewUser is called and username is medhat',
-        inject(function (UserService, $httpBackend) {
-            $httpBackend.expectPOST(httpApiServer + '/api/user');
-            $httpBackend.when('POST', httpApiServer + '/api/user').respond({});
-            UserService.createNewUser(
-                mockRegisterData.username,
-                mockRegisterData.email,
-                mockRegisterData.password,
-                jasmine.any('Function'));
-            // $httpBackend.flush();
 
-            // $httpBackend.verifyNoOutstandingExpectation();
-            // $httpBackend.verifyNoOutstandingRequest();
-        })
+    beforeEach(inject(function(_UserService_, _$httpBackend_) {
+        UserService = _UserService_;
+        $httpBackend = _$httpBackend_;
+
+        // to capture the GET requests for html files
+        $httpBackend.whenGET(/./)
+            .respond(200);
+    }));
+
+    it('Should issue a POST request to /api/user when createnewUser is called and username is medhat', function() {
+        var expectedData = function(data) {
+            return '{"username":"medhatdawoud","email":"medhatdawoud@gmail.com","password":123456}';
+        }
+
+        $httpBackend.expectPOST(/./, expectedData)
+            .respond(200);
+
+        UserService.createNewUser(mockRegisterData.username, mockRegisterData.email, mockRegisterData.password, function(data) {});
+
+        $httpBackend.flush();
+
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
+    }
     );
 
 
