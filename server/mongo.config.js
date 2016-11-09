f901d106c33b6b86bfb7dffb5ce1b59104a68b01
Medhat Dawoud <medhatdawoud@gmail.com>
2016-11-09 14:41:08 +0200

    Created migration for creating restaurants on deploy, close #16

--- server/mongo.config.js
+++ server/mongo.config.js
@@ -1,4 +1,6 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose'),
+    seeder = require('mongoose-seeder'),
+    data = require('./seeds/restaurants.json');
 
 if (process.env.NODE_ENV === 'production') {
     mongoose.connect('mongodb://heroku_ncj5lwf2:cc4bcejjtvbrk0pe51dg42kmsd@ds023458.mlab.com:23458/heroku_ncj5lwf2')
@@ -9,7 +11,12 @@ if (process.env.NODE_ENV === 'production') {
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function (callback) {
-  console.log("Connected to Database DailyMumm");
+db.once('open', function(callback) {
+    console.log("Connected to Database DailyMumm");
+    seeder.seed(data, { dropCollections: true }).then(function(dbData) {
+        console.log('Restaurants are stored in dbData');
+    }).catch(function(err) {
+        console.log('Error ' + err);
+    });
 });
 module.exports = mongoose;
