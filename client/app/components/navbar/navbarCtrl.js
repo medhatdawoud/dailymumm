4827457c129f9eedff94a8a9ad2eb4ad49ad2489
Medhat Dawoud <medhatdawoud@gmail.com>
2016-11-16 18:41:18 +0200

    increase timeout default to 5 minutes

--- client/app/components/navbar/navbarCtrl.js
+++ client/app/components/navbar/navbarCtrl.js
@@ -10,7 +10,7 @@
     function NavbarDirectiveController($scope, $rootScope, AuthService, $state, $timeout, CountDownService, CurrentOrderService) {
 
         // 15 second
-        var collectingOrderTime = 0.25 * 60 * 1000;
+        var collectingOrderTime = 5 * 60 * 1000;
 
         $scope.logout = logout;
         $scope.orderStarted = false;
