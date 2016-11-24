3f33a7c8a5344758b8dfbf7ba432d25125681bd9
Medhat Dawoud <medhatdawoud@gmail.com>
2016-11-24 11:28:50 +0200

    Added 404 page route

--- client/app/app.routes.js
+++ client/app/app.routes.js
@@ -4,7 +4,7 @@
     angular.module('dailyMummApp')
     .config(["$stateProvider", "$urlRouterProvider", "$translateProvider",
         function ($stateProvider, $urlRouterProvider, $translateProvider) {
-            $urlRouterProvider.otherwise('/');
+            $urlRouterProvider.otherwise('/404');
             $stateProvider
                 .state('home', {
                     url: '/',
@@ -45,7 +45,12 @@
                     url: '/edit',
                     templateUrl: 'pages/profile/profile.edit.html',
                     parent: 'profile'
-                });
+                })
+                .state('404', {
+                    url: '/404',
+                    templateUrl: '404.html',
+                })
+                ;
 
             $translateProvider.useUrlLoader('localization/en.json');
             $translateProvider.preferredLanguage('en');
